0ad0dffb1c7d98c9e9f9fcd94aa55a53fdf07796
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-06-23 01:02:35 +0900

    implement example more real usage

--- example/Binding/example.hs
+++ example/Binding/example.hs
@@ -2,25 +2,39 @@ import Bindings.Friso.Raw
 import Bindings.Friso.Util
 import Foreign.C.String
 import System.Exit
+import Control.Monad
 
 main :: IO ()
 main = do
   -- init friso
   friso <- c'friso_new
+  -- init friso config
   config <- c'friso_new_config
+
+  -- init with config
   configPath <- newCString "/etc/friso/friso.ini"
   init_result <- c'friso_init_from_ifile friso config configPath
   if init_result /= 1 then do
+    -- when it cannot initialize, abort.
     c'friso_free friso
     exitFailure
   else do
+    -- new friso task
     task <- c'friso_new_task
-    text <- newCString "要被分词的文本"
-    cstr <- peekCString text
-    putStrLn $ "original text: " ++ cstr
-    c'friso_set_text task text
-    putStrLn "result:"
-    c'friso_do_word friso config task
+    forever $ do
+      putStrLn "binding-friso>"
+      line <- getLine
+      when (line == "quit" || line == "q") $ do
+        putStrLn "quit"
+        exitSuccess
+      -- input to tokenize text
+      text <- newCString line
+      cstr <- peekCString text
+      putStrLn $ "original text: " ++ cstr
+      c'friso_set_text task text
+      putStrLn "result:"
+      -- execute tokenize
+      c'friso_do_word friso config task
     c'friso_free_task task
     c'friso_free friso
   return ()
