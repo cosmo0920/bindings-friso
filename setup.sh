927f742001b181c1103a9de4b9717db5228b54ba
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-07-03 09:10:51 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+wget https://friso.googlecode.com/files/friso-1.6.0-src-lib-dict.zip
+unzip friso-1.6.0-src-lib-dict.zip
+cd friso-1.6.0/src
+make
+sudo make install
+sudo mkdir /etc/friso
+sudo cp friso.ini /etc/friso
+sudo mkdir /usr/share/friso
+sudo cp -r dict /usr/share/friso
+sudo chmod 755 /usr/share/friso
